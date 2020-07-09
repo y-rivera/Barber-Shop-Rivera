ceac721abe44a39c3054e8e95340422031f49175
y-rivera <62513451+y-rivera@users.noreply.github.com>
2020-07-08 20:55:59 -0600

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,42 @@
+
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  let telefono = "50379902031";   
+
+  let cliente = document.querySelector("#cliente").value;
+  let fecha = document.querySelector("#fecha").value;
+  let hora = document.querySelector("#hora").value;
+  let empleado = document.querySelector("#empleado").value;
+  let servicio = document.querySelector("#servicio").value;
+  let resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  let url = `https://api.whatsapp.com/send?phone=50379902031&text=
+		*BARBER SHOP RIVERA*%0A
+		 Reservación Online%0A%0A
+		*¿Cuál es tu nombre?*%0A
+		${cliente}%0A
+		*Indica la fecha de tu reserva*%0A   
+		${fecha}%0A
+		*Indica la hora de tu reserva*%0A
+		${hora}%0A
+		*Empleado de preferencia*%0A    
+		${empleado}%0A
+		*¿Cuál es el servicio que se desea realizar?*%0A
+    ${servicio}`;
+
+  if (cliente === "" || fecha === "" || hora === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Su solicitud se ha enviado , ${cliente}`;
+
+  window.open(url);
+});
